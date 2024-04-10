fead04d13bc5c6faeb5b5bdd659c97034adee3db
cand0 <rudtjr7655@gmail.com>
2024-04-10 14:14:58 +0900

    [etc] Change Network Directory Module Name

---
+++ network_module/route.tf
@@ -0,0 +1,16 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.cand0.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cand0.id
+  }
+  tags = {
+    Name = var.project_name
+  }
+}
+
+resource "aws_route_table_association" "public" {
+    count = length(aws_subnet.public)
+    subnet_id = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
+}
