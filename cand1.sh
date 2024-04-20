55b125f7e89537b104de97c9e088189bc5240703
cand0 <rudtjr7655@gmail.com>
2024-04-20 11:27:16 +0900

    [feat] terraform code runs automatically

--- cand1.sh
+++ cand1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-aws eks --region ap-northeast-2 update-kubeconfig --name cand0-eks
-# aws eks --region ap-northeast-2 update-kubeconfig --name [Cluster Name]
+export TF_VAR_iam_user_id=""
+export cluster_name="cand0-eks"
+
+# terraform 경로 찾은 뒤 해당 경로에서 eks 실행
+pwd_dir="`pwd`/`dirname $0`/eks"
+cd $pwd_dir
+terraform apply --auto-approve
+
+
+# eks config 파일 수정
+aws eks --region ap-northeast-2 update-kubeconfig --name $cluster_name
+# aws eks --region ap-northeast-2 update-kubeconfig --name [cluster Name]
 
