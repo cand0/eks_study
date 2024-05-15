24427362b0e13a6893ee2714b5e6c4f2950da15b
cand0 <rudtjr7655@gmail.com>
2024-05-15 20:58:15 +0900

    [refactor] change directory path

--- cand1.sh
+++ cand1.sh
@@ -4,12 +4,12 @@ export TF_VAR_iam_user_id=""
 export cluster_name="cand0-eks"
 
 # terraform 경로 찾은 뒤 해당 경로에서 eks 실행
-pwd_dir="`pwd`/`dirname $0`/eks"
+pwd_dir="`pwd`/`dirname $0`/terraform/eks"
 cd $pwd_dir
 terraform apply --auto-approve
 
 
 # eks config 파일 수정
 aws eks --region ap-northeast-2 update-kubeconfig --name $cluster_name
-# aws eks --region ap-northeast-2 update-kubeconfig --name [cluster Name]
+
 
